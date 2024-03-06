150fd81d14cbe644f9917ae04b51fc406b448f2d
Oscar Moxon <oscarjmoxon@gmail.com>
2024-03-06 11:52:47 +0000

    Configured for deployment

---
+++ docs/_app/immutable/entry/start.CM57pY__.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BGgu8u3z.js";export{t as start};
---
+++ docs/_app/immutable/nodes/0.BmZOf6rJ.js
@@ -0,0 +1,18 @@
+import{s as E,n as y,b as T,u as N,g as S,d as V}from"../chunks/scheduler.Cs0xm5t1.js";import{S as H,i as L,e as f,c as p,l as M,k as d,g,d as r,o as q,s as x,p as I,f as b,q as z,a as K,r as Y,h as v,u as C,v as A,w as j}from"../chunks/index.DVZDcXYT.js";function B(c){let t,i='<div class="left svelte-1upai6a"><a href="/" class="home-link svelte-1upai6a">[oscar moxon]</a></div> <div class="center svelte-1upai6a"><a href="/essays" class="svelte-1upai6a">[essays]</a> <a href="/contact" class="svelte-1upai6a">[contact]</a> <a href="/about" class="svelte-1upai6a">[about]</a></div> <div class="right svelte-1upai6a"><a href="/CV.pdf" target="_blank" rel="noopener noreferrer" class="cv-link svelte-1upai6a">CV</a></div>';return{c(){t=f("nav"),t.innerHTML=i,this.h()},l(a){t=p(a,"NAV",{class:!0,"data-svelte-h":!0}),M(t)!=="svelte-143y6e2"&&(t.innerHTML=i),this.h()},h(){d(t,"class","header svelte-1upai6a")},m(a,o){g(a,t,o)},p:y,i:y,o:y,d(a){a&&r(t)}}}class D extends H{constructor(t){super(),L(this,t,null,B,E,{})}}function F(c){let t,i,a,o,l,u,w=`body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      background-color: #fafafa;
+    }
+
+    .central-column {
+      max-width: 600px; /* or the width you desire */
+      margin: 0 auto; /* centers the column */
+      padding: 2rem; /* adds some padding around the content */
+    }
+
+    .contact-column {
+      max-width: 400px; /* or the width you desire */
+      margin: 0 auto; /* centers the column */
+      padding: 2rem; /* adds some padding around the content */
+    }`,_,m,h;t=new D({});const $=c[1].default,n=T($,c,c[0],null);return{c(){q(t.$$.fragment),i=x(),a=f("meta"),o=f("meta"),l=f("link"),u=f("style"),u.textContent=w,_=x(),m=f("main"),n&&n.c(),this.h()},l(e){I(t.$$.fragment,e),i=b(e);const s=z("svelte-1bkwakn",document.head);a=p(s,"META",{charset:!0}),o=p(s,"META",{name:!0,content:!0}),l=p(s,"LINK",{rel:!0,type:!0,href:!0}),u=p(s,"STYLE",{"data-svelte-h":!0}),M(u)!=="svelte-1ar6mwz"&&(u.textContent=w),s.forEach(r),_=b(e),m=p(e,"MAIN",{});var k=K(m);n&&n.l(k),k.forEach(r),this.h()},h(){d(a,"charset","utf-8"),d(o,"name","viewport"),d(o,"content","width=device-width,initial-scale=1"),document.title="[oscar moxon]",d(l,"rel","icon"),d(l,"type","image/png"),d(l,"href","/knotonnothing.png")},m(e,s){Y(t,e,s),g(e,i,s),v(document.head,a),v(document.head,o),v(document.head,l),v(document.head,u),g(e,_,s),g(e,m,s),n&&n.m(m,null),h=!0},p(e,[s]){n&&n.p&&(!h||s&1)&&N(n,$,e,e[0],h?V($,e[0],s,null):S(e[0]),null)},i(e){h||(C(t.$$.fragment,e),C(n,e),h=!0)},o(e){A(t.$$.fragment,e),A(n,e),h=!1},d(e){e&&(r(i),r(_),r(m)),j(t,e),r(a),r(o),r(l),r(u),n&&n.d(e)}}}function G(c,t,i){let{$$slots:a={},$$scope:o}=t;return c.$$set=l=>{"$$scope"in l&&i(0,o=l.$$scope)},[o,a]}class P extends H{constructor(t){super(),L(this,t,G,F,E,{})}}export{P as component};
