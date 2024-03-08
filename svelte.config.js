cd4e22f35d91c1eb1d7f575ae73722291bf3b27f
Oscar Moxon <oscarjmoxon@gmail.com>
2024-03-08 15:24:41 +0000

    First working sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-static';
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    // Ensure paths are relative for GitHub Pages
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/blog-olympia' : '',
+    },
+    // Other configurations...
+  },
 };
-
-export default config;
