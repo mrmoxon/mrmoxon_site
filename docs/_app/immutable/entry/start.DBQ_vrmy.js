df60a554adcef2f6bbd95bee9442597942f98f74
Oscar Moxon <oscarjmoxon@gmail.com>
2024-03-08 20:05:13 +0000

    fuck

--- docs/_app/immutable/entry/start.DBQ_vrmy.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.wOaN-JN6.js";export{t as start};
--- src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-// src/lib/utils.ts
-export function formatDate(date: Date): string {
-    // Simple date formatting function
-    return date.toISOString().substring(0, 10);
-  }
-  
